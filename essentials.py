eee2b386640b15c4d87e6e4806f9141a6270e6cc
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-24 09:49:09 -0500

    project started

--- essentials.py
+++ essentials.py
@@ -1,1 +1,24 @@
+# TYPES
+"strings"
+17 # numbers
+17.5 #floats
+False, True # Booleans
+["Lenovo", "Apple", "Razer", "Logitech", "RTX"] # Arrays
+
+# ---------------------BUT WHERE DO WE STORE THEM ?----------------------VARIABLES!!!-------------------------
+
 # Variables | Remeber to name them with names that make sense
+user_name = "Comanche"
+user_height = 1.78
+memory1 = 16
+computers = ["Lenovo", "Apple", "Razer", "Logitech", "RTX"]
+variable_1 = user_name
+
+# Variables names you can't use
+_number = 21 # Putting Dash before variable name
+1number = 21 # Puttung number before variable name
+
+# ---------------------BUT HOW DO WE SHOW THEM?----------------------------PRINT IN CONSOLE!!!------------------
+print(user_name)
+
+# To run a program open your terminal, type and type ------------>  python3 essentials.py
