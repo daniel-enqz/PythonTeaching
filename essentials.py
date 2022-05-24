91793ce445ad5379dab8a9bdc0f6d0966863c04a
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-24 10:01:02 -0500

    project started

--- essentials.py
+++ essentials.py
@@ -8,7 +8,7 @@ False, True # Booleans
 # ---------------------BUT WHERE DO WE STORE THEM ?----------------------VARIABLES!!!-------------------------
 
 # Variables | Remeber to name them with names that make sense
-user_name = "Comanche"
+user_name = "Pana"
 user_height = 1.78
 memory1 = 16
 computers = ["Lenovo", "Apple", "Razer", "Logitech", "RTX"]
@@ -16,9 +16,19 @@ variable_1 = user_name
 
 # Variables names you can't use
 _number = 21 # Putting Dash before variable name
-1number = 21 # Puttung number before variable name
+number = 21 # Puttung number before variable name
+
+# Remember variables can overwrite themselves, like in the next example
+user_name = "Comanche"
+
 
 # ---------------------BUT HOW DO WE SHOW THEM?----------------------------PRINT IN CONSOLE!!!------------------
 print(user_name)
+# You can print whatever you want, not only variables!!!
+
 
 # To run a program open your terminal, type and type ------------>  python3 essentials.py
+
+# ---------------------CAN WE GET INPUT FROM USER?------------------------YES!!! WITH INPUT------------------
+user_name = (input("What's your name\n---->"))
+print(user_name)
