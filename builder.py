dedccb0bb508828ac925aa84a4c5adee0110a165
Daniel Enriquez <daniel.enm17@gmail.com>
2022-05-25 09:06:37 -0500

    project finished

--- builder.py
+++ builder.py
@@ -14,12 +14,12 @@ class color:
 # Options
 parts = {
   "CPU": [["Rayzen7", 5000], ["CoreI7", 6000], ["Rayzen5", 2000]],
-  "MOTHERBOARD": ["Gigabyte Z690 Aorus Pro", "ASRock Z690 Taichi"],
-  "GPU_GRAPHIC_CARD": ["Nvidia GeForce RTX 3090 Ti", "Nvidia GeForce RTX 3080"],
-  "RAM": [8, 16, 32],
-  "PRIMARY_STORAGE_SSD": ["WD Black SN850", "WD Black SN770"],
-  "PSU": ["Corsair RM750x", "Seasonic Prime Titanium TX-1000"], # Power Supply
-  "CASE": ["Fractal Design Meshify 2 Compact", "Lian Li O11 Air Mini"]
+  "MOTHERBOARD": [["Gigabyte Z690 Aorus Pro", 7000], ["ASRock Z690 Taichi", 10000]],
+  "GPU_GRAPHIC_CARD": [["Nvidia GeForce RTX 3090 Ti", 32000], ["Nvidia GeForce RTX 3080", 16000]],
+  "RAM": [[8, 4500], [16, 5000], [32, 10000]],
+  "PRIMARY_STORAGE_SSD": [["WD Black SN850", 7250], ["WD Black SN770", 8345]],
+  "PSU": [["Corsair RM750x", 13200], ["Seasonic Prime Titanium TX-1000", 8450]], # Power Supply
+  "CASE": [["Fractal Design Meshify 2 Compact", 3400], ["Lian Li O11 Air Mini", 5500]]
 }
 order = ["CPU", "MOTHERBOARD", "GPU_GRAPHIC_CARD", "RAM", "PRIMARY_STORAGE_SSD", "PSU", "CASE"]
 
@@ -34,8 +34,8 @@ for i in range(len(parts)):
     current_part = order[i]
     print("Choose your {}".format(current_part))
     for i in range(len(parts[current_part])):
-        print([i + 1], parts[current_part][i][i], "Price:", parts[current_part][i][i+1])
-    user_choice = input("----->")
+        print([i + 1], parts[current_part][i][0], "Price:", parts[current_part][i][1])
+    user_choice = int(input("----->"))
     user_parts.append(parts[current_part][user_choice-1][0])
     user_price += parts[current_part][user_choice-1][1]
 # Final output
