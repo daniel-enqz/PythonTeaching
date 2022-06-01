669a6c7366aaa7fb7342c4ed7ee283c5a4da716f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-06-01 12:26:48 -0500

    User Roles Advacning

--- essentials.py
+++ essentials.py
@@ -1,9 +1,9 @@
 # TYPES
 "strings"
-17 # numbers
+17 # numbers # Integers
 17.5 #floats
 False, True # Booleans
-["Lenovo", "Apple", "Razer", "Logitech", "RTX"] # Arrays
+["Lenovo", 17, "Razer", True, "RTX"] # Arrays
 
 # ---------------------BUT WHERE DO WE STORE THEM ?----------------------VARIABLES!!!-------------------------
 
@@ -15,7 +15,7 @@ computers = ["Lenovo", "Apple", "Razer", "Logitech", "RTX"]
 variable_1 = user_name
 
 # Variables names you can't use
-_number = 21 # Putting Dash before variable name
+number = 21 # Putting Dash before variable name
 number = 21 # Puttung number before variable name
 
 # Remember variables can overwrite themselves, like in the next example
@@ -23,15 +23,22 @@ user_name = "Comanche"
 
 
 # ---------------------BUT HOW DO WE SHOW THEM?----------------------------PRINT IN CONSOLE!!!------------------
-# print(user_name)
+print(user_name)
+print("Welcome to PC Builder")
 # You can print whatever you want, not only variables!!!
 
 
 # To run a program open your terminal, type and type ------------>  python3 essentials.py
 
 # ---------------------CAN WE GET INPUT FROM USER?------------------------YES!!! WITH INPUT------------------
-user_name = input("What's your name\n---->")
+# user_name = input("What's your name\n---->")
 # print(user_name)
+
+user_name = input("Cuál es tu nombre?\n----->")
+
+if user_name == "Comanche":
+
+print(f"Welcome to PC Builder, {user_name}")
 
 # LAST BUT NOT LEAST, SOME LOGIC....
 if user_name == "Santiago":
